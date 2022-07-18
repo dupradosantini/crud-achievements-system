efd353ed29f563de33799dcfd29ca9034b7dca0a
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-07-18 11:21:37 -0300

    Defining in memory user and password encoder.

--- src/main/java/dupradosantini/achievementsystem/security/ApplicationSecurityConfig.java
+++ src/main/java/dupradosantini/achievementsystem/security/ApplicationSecurityConfig.java
@@ -1,14 +1,27 @@
 package dupradosantini.achievementsystem.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,5 +32,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated() // has to be authenticated
                 .and()
                 .httpBasic(); //and using basic auth.
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        // Hard defining a user
+        //Must define the password encoder as well
+        UserDetails luisUser = User.builder()
+                .username("luis")
+                .password(passwordEncoder.encode("password"))
+                .roles("DEV") //ROLE_DEV (how springSec understands this role)
+                .build();
+        return new InMemoryUserDetailsManager(luisUser);
     }
 }
